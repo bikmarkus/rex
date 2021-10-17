ac552a231803cff72c6a8aae6dd0b683ad0a8b0c
Linar Biktimirov <botnick912@gmail.com>
2021-10-17 20:30:55 +0400

    modify title and fixed bug to proper project id transfer

--- app/controllers/ProjectCardController.php
+++ app/controllers/ProjectCardController.php
@@ -10,12 +10,14 @@ class ProjectCardController
      * Show current card
      */
     public function index()
-    {     
-        session_start();
-        if($_SESSION['project_id']!=''){
-            $card_id = $_SESSION['project_id'];
-        } else {
+    {   
+        session_start();  
+        if($_POST['card_id']!='')
+        {
             $card_id = $_POST['card_id'];
+        } elseif($_SESSION['project_id']!='') 
+        {
+            $card_id = $_SESSION['project_id'];
         }
         $card = App::get('database')->selectAllById('projects',$card_id);
         $card_next = App::get('database')->selectAllNextByProjectId("next",$card_id);
--- app/views/partials/head.php
+++ app/views/partials/head.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Dog takes under control</title>
+    <title>Dog takes over control</title>
     <link rel="stylesheet" href="public/css/style.css">
     <link rel="shortcut icon" href="favicon.png" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1">
