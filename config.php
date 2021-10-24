368a3525d385e1b265dc0fa53684a915b205ae1c
Linar Biktimirov <botnick912@gmail.com>
2021-10-24 18:59:49 +0400

    gitignore updated

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
         'database' => [
             'name' => 'mytodo',
             'username' => 'root',
-            'password' => '',
+            'password' => 'root',
             'connection' => 'mysql:host=127.0.0.1',
             'options' => [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
